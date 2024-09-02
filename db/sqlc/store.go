18b737e85f1d51de47fd17c23fdfc1c9af1213f4
The-Anton <shesingh@redhat.com>
2024-09-02 22:19:19 +0530

    add: transaction implementation

---
+++ db/sqlc/store.go
@@ -0,0 +1,87 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"log"
+)
+
+type Store struct {
+	*Queries 
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{db: db, Queries: New(db)}
+}
+
+func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := store.db.BeginTx(ctx, nil)
+	if err != nil {
+		return err
+	}
+
+	q := New(tx)
+	err = fn(q)
+	if err != nil {
+		if rbErr := tx.Rollback(); rbErr != nil {
+			log.Fatal("tx error %v, rollback error %v", err, rbErr)
+		}
+	}
+
+	return tx.Commit()
+}
+
+type TransferTxResult struct {
+	Transfer Transfer `json:"transfer"`
+	SenderAccount Account `json:"from_account"`
+	ReceiverAccount Account `json:"to_account"`
+	SenderEntry Entry `json:"from_entry"`
+	ReceiverEntry Entry `json:"to_entry"`
+}
+
+type TransferTxInput struct {
+	SenderID int64 `json:"sender_id"`
+	ReceiverID int64	`json:"reciever_id"`
+	Amount int64 `json:"amount"`
+}
+
+
+
+func (store *Store) TransferTx(ctx context.Context, arg TransferTxInput) (TransferTxResult, error) {
+	var result TransferTxResult
+
+	err := store.execTx(ctx, func (q *Queries) error {
+		var err error
+		
+		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
+			SenderID: arg.SenderID,
+			ReceiverID: arg.ReceiverID,
+			Amount: arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.SenderEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.ReceiverID,
+			Amount: -arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		result.ReceiverEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.SenderID,
+			Amount: arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+
+		return nil
+	})
+
+	return result, err
+}
